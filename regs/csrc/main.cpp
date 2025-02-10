26660e77e03e7d181d9e88c21d3186f24079cb74
Rain <2190155325@qq.com>
2025-02-10 23:36:54 +0800

    inplement a reg

---
+++ regs/csrc/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
+
+#include <nvboard.h>
+#include <Vtop.h>
+
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+
+int main(int argc, char **argv) {
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    dut.clk = 0;
+
+    while (1) {
+        dut.clk = ~dut.clk;
+        nvboard_update();
+        dut.eval();
+    }
+
+    return 0;
+}
